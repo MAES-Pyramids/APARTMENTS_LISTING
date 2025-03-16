3f2b4b14bc008eec11513b9bd023101d4bed62e1
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2025-03-16 23:33:38 +0200

    chore(docker): update backend API image to use latest version

--- frontend/.next/server/middleware-build-manifest.js
+++ frontend/.next/server/middleware-build-manifest.js
@@ -1,1 +1,22 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-c23f2069c2e0d161.js","static/chunks/4bd1b696-89ff0aafb1460fb1.js","static/chunks/684-6072daede04738b2.js","static/chunks/main-app-59c2ba113cb54688.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-c23f2069c2e0d161.js","static/chunks/framework-859199dea06580b0.js","static/chunks/main-1d469d2c6fe8888b.js","static/chunks/pages/_app-a66f9296699c5863.js"],"/_error":["static/chunks/webpack-c23f2069c2e0d161.js","static/chunks/framework-859199dea06580b0.js","static/chunks/main-1d469d2c6fe8888b.js","static/chunks/pages/_error-7688f4c9a69e67c8.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
